79f48bbd211c68d978410067ee9c42e0251fe019
xiaoshukeji <119823795+xiaoshukeji@users.noreply.github.com>
2023-02-21 23:08:56 +0800

    更新

--- files/main.js
+++ files/main.js
@@ -1,7 +1,7 @@
 
     
     function play(){
-    swal("您好，感谢您的下载，但免费加速器运营成本极高，使用夸克下载可以助力我们运营，您是否需要继续下载？十分感谢主子的理解与支持!", {
+    swal("您好，感谢您的下载，但本站运营成本极高，使用副站下载可以助力我们运营，您是否需要继续下载？十分感谢主子的理解与支持!", {
             buttons: {
                 cancel: "取消下载",
                 allow: "立即下载"
@@ -9,7 +9,7 @@
         }).then(function(value) {
             if (value == "allow") {
               
-              window.open("https://pan.quark.cn/s/eee45f75d8b0") 
+              window.open("https://xiaoshukeji.github.io/X-Blog/xs-win/index.html") 
               
             } else {
         
