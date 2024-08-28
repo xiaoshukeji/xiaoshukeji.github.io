c239bb8acd86562fe41166335d8218ebed6bb4c1
xiaoshukeji <13644725699@163.com>
2024-08-28 22:09:08 +0800

    updata

---
+++ mh/pythonProject/123.py
@@ -0,0 +1,4 @@
+num = []
+for i in range(36):
+    num.append(i+1)
+print(num)
---
+++ mh/pythonProject/main.py
@@ -0,0 +1,12 @@
+
+num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60]
+for i in range(36):
+    print("<div class="+'"'+"order-0 order-sm-0 order-md-0 order-lg-0 order-xl-0 col-12 col-md-4 col-lg-3 col-xl-2"+'"'+">")
+    print("<div class="+'"'+"video-block video-with-trailer"+'"'+" data-post-id="+'"'+"133210"+'"'+">")
+    print("<span class="+'"'+"thumb"+'"'+">")
+    print("<div class="+'"'+"video-debounce-bar"+'"'+"></div>")
+    print("<img src="+'"'+"../a_shu/video ("+str(num[i])+").png"+'"'+" alt="+'"'+"阿树视频缩略图"+'"'+">")
+    print("</span>")
+    print("<span class="+'"'+"title"+'"'+">阿树图片</span>")
+    print("</div>")
+    print("</div>")
